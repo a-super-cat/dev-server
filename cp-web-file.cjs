8c3319d2dc49a20ac78c043c0f666791c8481294
贾志伟 <2405015284@qq.com>
2024-04-06 21:52:30 +0800

    fix

--- cp-web-file.cjs
+++ cp-web-file.cjs
@@ -1,5 +1,5 @@
 const path = require('path');
 const fs = require('fs-extra');
-const webFilePath = path.join(__dirname, './node_modules/@jiazhiwei/dev-server-web/dist/');
+const webFilePath = path.join(__dirname, './node_modules/@easily-tools/mock-server-web/dist/');
 
 fs.copy(webFilePath, path.join(__dirname, './web/'), (err) => {})
